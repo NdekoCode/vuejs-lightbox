8617537808760809b20b15a69c60aba126e76150
NdekoCode <arickbulakali@gmail.com>
2023-02-08 08:53:02 +0200

    style: responsive close icon

--- src/components/Lightbox/Lightbox.vue
+++ src/components/Lightbox/Lightbox.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="lightbox" v-if="image" @click="close">
     <LightboxImage :image="image" />
-    <button class="lightbox__close" @click.prevent="close"></button>
+    <div class="lightbox__close-container">
+      <button class="lightbox__close" @click.prevent="close">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="ionicon"
+          viewBox="0 0 512 512"
+        >
+          <title>Close</title>
+          <path
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="25"
+            d="M368 368L144 144M368 144L144 368"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
 
